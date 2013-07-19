0a9b95877e6597ef148a6da8c516f00151ce4853
Adam Brown <adamwbrown@gmail.com>
2013-07-19 00:22:33 -0700

    - Parsing wikipedia summary paragraphs now works! It's still using the hardcoded Falcon_9 article for now though.

--- TMinus/src/main/java/com/darkrockstudios/apps/tminus/RocketDetailFragment.java
+++ TMinus/src/main/java/com/darkrockstudios/apps/tminus/RocketDetailFragment.java
@@ -6,6 +6,8 @@ import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +20,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
-import com.darkrockstudios.apps.tminus.R.string;
 import com.darkrockstudios.apps.tminus.database.DatabaseHelper;
 import com.darkrockstudios.apps.tminus.launchlibrary.Rocket;
 import com.darkrockstudios.apps.tminus.misc.DiskBitmapCache;
@@ -31,6 +32,8 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Adam on 7/14/13.
@@ -39,13 +42,23 @@ import java.sql.SQLException;
  */
 public class RocketDetailFragment extends DialogFragment
 {
-	public static final String TAG         = LaunchDetailFragment.class.getSimpleName();
-	public static final String ARG_ITEM_ID = "item_id";
+	public static final String TAG                   = LaunchDetailFragment.class.getSimpleName();
+	public static final String ARG_ITEM_ID           = "item_id";
+	final               String hardCodedArticleTitle = "Falcon_9";
+
+	private static final Pattern COMMENT_PATTERN = Pattern.compile( "\\<\\!--(.*?)--\\>" );
+	private static final Pattern LINK_PATTERN    = Pattern.compile( "\\[\\[(.*?:)?(.*?)(\\|.*?)?\\]\\]" );
+	private static final Pattern FILE_PATTERN    = Pattern.compile( "\\[\\[File:(.*?)\\]\\]" );
+	private static final Pattern CONVERT_PATTERN = Pattern.compile( "\\{\\{convert\\|([0-9]+)\\|([a-zA-Z]+)\\}\\}" );
+	private static final Pattern BOLD_PATTERN    = Pattern.compile( "'''(.+?)'''" );
+	private static final Pattern ITALICS_PATTERN = Pattern.compile( "''(.+?)''" );
+
 	private File             m_dataDirectory;
 	private Rocket           m_rocket;
 	private NetworkImageView m_rocketImage;
 	private TextView         m_rocketName;
 	private TextView         m_rocketConfiguration;
+	private TextView m_rocketDetails;
 
 	public RocketDetailFragment()
 	{
@@ -76,7 +89,7 @@ public class RocketDetailFragment extends DialogFragment
 		Dialog dialog = getDialog();
 		if( dialog != null )
 		{
-			dialog.setTitle( string.ROCKETDETAIL_title );
+			dialog.setTitle( R.string.ROCKETDETAIL_title );
 		}
 
 		View rootView = inflater.inflate( R.layout.fragment_rocket_detail, container, false );
@@ -86,6 +99,7 @@ public class RocketDetailFragment extends DialogFragment
 			m_rocketImage = (NetworkImageView)rootView.findViewById( R.id.ROCKETDETAIL_rocket_image );
 			m_rocketName = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_name );
 			m_rocketConfiguration = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_configuration );
+			m_rocketDetails = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_details );
 
 			loadRocket();
 		}
@@ -141,8 +155,6 @@ public class RocketDetailFragment extends DialogFragment
 		// Action: query
 		// prop=pageimages
 
-		final String hardCodedArticleTitle = "Falcon_9";
-
 		final String baseUrl = "http://en.wikipedia.org";
 		final String imageQuery = "/w/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail%7Cname&pithumbsize=512&pilimit=1&indexpageids=&titles=";
 
@@ -156,6 +168,119 @@ public class RocketDetailFragment extends DialogFragment
 
 	// This Wiki api call will get the summary section for a given article
 	// /w/api.php?action=parse&format=json&page=Falcon_9&prop=text&section=0
+
+	// the Parse tree option might be what we want, it allows us to traverse the content via XML rather than regex that crap
+
+	private void requestRocketArticle()
+	{
+		final String baseUrl = "http://en.wikipedia.org";
+		final String articleQuery = "/w/api.php?action=parse&format=json&prop=wikitext&section=0&contentformat=text%2Fx-wiki&contentmodel=wikitext&page=";
+
+		final String url = baseUrl + articleQuery + hardCodedArticleTitle;
+
+		WikiArticleListener listener = new WikiArticleListener( m_rocketDetails );
+		JsonObjectRequest request = new JsonObjectRequest( url, null, listener, listener );
+		request.setTag( this );
+		TMinusApplication.getRequestQueue().add( request );
+	}
+
+	private static class WikiArticleListener implements Listener<JSONObject>, ErrorListener
+	{
+		private TextView m_articleText;
+
+		public WikiArticleListener( TextView articleText )
+		{
+			m_articleText = articleText;
+		}
+
+		@Override
+		public void onResponse( JSONObject response )
+		{
+			try
+			{
+				JSONObject parse = response.getJSONObject( "parse" );
+				JSONObject text = parse.getJSONObject( "wikitext" );
+
+				String articleText = text.getString( "*" );
+				articleText = cleanUpWikiText( articleText );
+
+				Spanned htmlText = Html.fromHtml( articleText );
+				m_articleText.setText( htmlText );
+			}
+			catch( JSONException e )
+			{
+				e.printStackTrace();
+			}
+		}
+
+		@Override
+		public void onErrorResponse( VolleyError error )
+		{
+
+		}
+
+		private String cleanUpWikiText( final String wikiText )
+		{
+			// Unescape the wikitext
+			String articleText = wikiText.replace( "\\\"", "\"" );
+
+			articleText = removeInfoBox( articleText );
+
+			// Use our regex patterns to clean out the wiki syntax and replace it with mostly plain text
+			// or simple HTML for formatting
+			Matcher matcher = COMMENT_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "" );
+
+			matcher = FILE_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "" );
+
+			matcher = LINK_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "$2" );
+
+			matcher = CONVERT_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "$1 $2" );
+
+			matcher = BOLD_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "<strong>$1</strong>" );
+
+			matcher = ITALICS_PATTERN.matcher( articleText );
+			articleText = matcher.replaceAll( "<em>$1</em>" );
+
+			// Lastly trim any whitespace, and then space things out
+			articleText = articleText.trim();
+			articleText = articleText.replace( "\n", "<br/>" );
+
+			return articleText;
+		}
+
+		private String removeInfoBox( final String articleText )
+		{
+			// Find the end of the Infobox
+			final String infoBox = "{{Infobox";
+			final String openBracket = "{{";
+			final String closeBracket = "}}";
+			int curPos = articleText.indexOf( infoBox ) + infoBox.length();
+			int openBrackets = 1;
+			while( openBrackets > 0 )
+			{
+				int nextOpen = articleText.indexOf( openBracket, curPos );
+				int nextClose = articleText.indexOf( closeBracket, curPos );
+				if( nextOpen < nextClose )
+				{
+					curPos = nextOpen + openBracket.length();
+					++openBrackets;
+				}
+				else
+				{
+					curPos = nextClose + closeBracket.length();
+					--openBrackets;
+				}
+			}
+
+			// Grab everything immediately after the Infobox
+			return articleText.substring( curPos, articleText.length() );
+		}
+	}
 
 	private static class WikiImageListener implements Listener<JSONObject>, ErrorListener
 	{
@@ -252,6 +377,7 @@ public class RocketDetailFragment extends DialogFragment
 			if( m_rocket != null )
 			{
 				requestRocketImage();
+				requestRocketArticle();
 
 				updateViews();
 			}
