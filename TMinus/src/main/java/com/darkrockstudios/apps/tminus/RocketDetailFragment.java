14d80e5a7f7956ebb690e12ccf0817835d192a1d
Adam Brown <adamwbrown@gmail.com>
2013-07-22 23:04:10 -0700

    - Rocket Details now loaded from the web in a more production ready way (saved to DB and such)

--- TMinus/src/main/java/com/darkrockstudios/apps/tminus/RocketDetailFragment.java
+++ TMinus/src/main/java/com/darkrockstudios/apps/tminus/RocketDetailFragment.java
@@ -7,33 +7,27 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.Html;
-import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.android.volley.Response.ErrorListener;
-import com.android.volley.Response.Listener;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
 import com.darkrockstudios.apps.tminus.database.DatabaseHelper;
+import com.darkrockstudios.apps.tminus.database.RocketDetail;
 import com.darkrockstudios.apps.tminus.launchlibrary.Rocket;
+import com.darkrockstudios.apps.tminus.loaders.RocketDetailFetcher;
+import com.darkrockstudios.apps.tminus.loaders.RocketDetailFetcher.RocketDetailFetchListener;
+import com.darkrockstudios.apps.tminus.loaders.RocketDetailLoader;
+import com.darkrockstudios.apps.tminus.loaders.RocketDetailLoader.Listener;
 import com.darkrockstudios.apps.tminus.misc.DiskBitmapCache;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.Dao;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.File;
 import java.sql.SQLException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by Adam on 7/14/13.
@@ -42,23 +36,15 @@ import java.util.regex.Pattern;
  */
 public class RocketDetailFragment extends DialogFragment
 {
-	public static final String TAG                   = LaunchDetailFragment.class.getSimpleName();
-	public static final String ARG_ITEM_ID           = "item_id";
-	final               String hardCodedArticleTitle = "Falcon_9";
-
-	private static final Pattern COMMENT_PATTERN = Pattern.compile( "\\<\\!--(.*?)--\\>" );
-	private static final Pattern LINK_PATTERN    = Pattern.compile( "\\[\\[(.*?:)?(.*?)(\\|.*?)?\\]\\]" );
-	private static final Pattern FILE_PATTERN    = Pattern.compile( "\\[\\[File:(.*?)\\]\\]" );
-	private static final Pattern CONVERT_PATTERN = Pattern.compile( "\\{\\{convert\\|([0-9]+)\\|([a-zA-Z]+)\\}\\}" );
-	private static final Pattern BOLD_PATTERN    = Pattern.compile( "'''(.+?)'''" );
-	private static final Pattern ITALICS_PATTERN = Pattern.compile( "''(.+?)''" );
-
+	public static final String TAG         = LaunchDetailFragment.class.getSimpleName();
+	public static final String ARG_ITEM_ID = "item_id";
 	private File             m_dataDirectory;
 	private Rocket           m_rocket;
+	private RocketDetail m_rocketDetail;
 	private NetworkImageView m_rocketImage;
 	private TextView         m_rocketName;
 	private TextView         m_rocketConfiguration;
-	private TextView m_rocketDetails;
+	private TextView     m_rocketSummary;
 
 	public RocketDetailFragment()
 	{
@@ -99,7 +85,7 @@ public class RocketDetailFragment extends DialogFragment
 			m_rocketImage = (NetworkImageView)rootView.findViewById( R.id.ROCKETDETAIL_rocket_image );
 			m_rocketName = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_name );
 			m_rocketConfiguration = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_configuration );
-			m_rocketDetails = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_details );
+			m_rocketSummary = (TextView)rootView.findViewById( R.id.ROCKETDETAIL_details );
 
 			loadRocket();
 		}
@@ -122,6 +108,15 @@ public class RocketDetailFragment extends DialogFragment
 		{
 			m_rocketName.setText( m_rocket.name );
 			m_rocketConfiguration.setText( m_rocket.configuration );
+
+			if( m_rocketDetail != null )
+			{
+				final int MAX_CACHE_SIZE = 10 * 1024 * 1024;
+				ImageLoader imageLoader = new ImageLoader( TMinusApplication
+						                                           .getRequestQueue(), new DiskBitmapCache( m_dataDirectory, MAX_CACHE_SIZE ) );
+				m_rocketImage.setImageUrl( m_rocketDetail.imageUrl, imageLoader );
+				m_rocketSummary.setText( Html.fromHtml( m_rocketDetail.summary ) );
+			}
 		}
 	}
 
@@ -151,196 +146,7 @@ public class RocketDetailFragment extends DialogFragment
 		return rocketId;
 	}
 
-	private void requestRocketImage()
-	{
-		// This gives the "page image"
-		// Action: query
-		// prop=pageimages
-
-		final String baseUrl = "http://en.wikipedia.org";
-		final String imageQuery = "/w/api.php?action=query&prop=pageimages&format=json&piprop=thumbnail%7Cname&pithumbsize=512&pilimit=1&indexpageids=&titles=";
-
-		final String url = baseUrl + imageQuery + hardCodedArticleTitle;
-
-		WikiImageListener listener = new WikiImageListener( m_rocketImage, m_dataDirectory );
-		JsonObjectRequest request = new JsonObjectRequest( url, null, listener, listener );
-		request.setTag( this );
-		TMinusApplication.getRequestQueue().add( request );
-	}
-
-	// This Wiki api call will get the summary section for a given article
-	// /w/api.php?action=parse&format=json&page=Falcon_9&prop=text&section=0
-
-	// the Parse tree option might be what we want, it allows us to traverse the content via XML rather than regex that crap
-
-	private void requestRocketArticle()
-	{
-		final String baseUrl = "http://en.wikipedia.org";
-		final String articleQuery = "/w/api.php?action=parse&format=json&prop=wikitext&section=0&contentformat=text%2Fx-wiki&contentmodel=wikitext&page=";
-
-		final String url = baseUrl + articleQuery + hardCodedArticleTitle;
-
-		WikiArticleListener listener = new WikiArticleListener( m_rocketDetails );
-		JsonObjectRequest request = new JsonObjectRequest( url, null, listener, listener );
-		request.setTag( this );
-		TMinusApplication.getRequestQueue().add( request );
-	}
-
-	private static class WikiArticleListener implements Listener<JSONObject>, ErrorListener
-	{
-		private TextView m_articleText;
-
-		public WikiArticleListener( TextView articleText )
-		{
-			m_articleText = articleText;
-		}
-
-		@Override
-		public void onResponse( JSONObject response )
-		{
-			try
-			{
-				JSONObject parse = response.getJSONObject( "parse" );
-				JSONObject text = parse.getJSONObject( "wikitext" );
-
-				String articleText = text.getString( "*" );
-				articleText = cleanUpWikiText( articleText );
-
-				Spanned htmlText = Html.fromHtml( articleText );
-				m_articleText.setText( htmlText );
-			}
-			catch( JSONException e )
-			{
-				e.printStackTrace();
-			}
-		}
-
-		@Override
-		public void onErrorResponse( VolleyError error )
-		{
-
-		}
-
-		private String cleanUpWikiText( final String wikiText )
-		{
-			// Unescape the wikitext
-			String articleText = wikiText.replace( "\\\"", "\"" );
-
-			articleText = removeInfoBox( articleText );
-
-			// Use our regex patterns to clean out the wiki syntax and replace it with mostly plain text
-			// or simple HTML for formatting
-			Matcher matcher = COMMENT_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "" );
-
-			matcher = FILE_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "" );
-
-			matcher = LINK_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "$2" );
-
-			matcher = CONVERT_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "$1 $2" );
-
-			matcher = BOLD_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "<strong>$1</strong>" );
-
-			matcher = ITALICS_PATTERN.matcher( articleText );
-			articleText = matcher.replaceAll( "<em>$1</em>" );
-
-			// Lastly trim any whitespace, and then space things out
-			articleText = articleText.trim();
-			articleText = articleText.replace( "\n", "<br/>" );
-
-			return articleText;
-		}
-
-		private String removeInfoBox( final String articleText )
-		{
-			// Find the end of the Infobox
-			final String infoBox = "{{Infobox";
-			final String openBracket = "{{";
-			final String closeBracket = "}}";
-			int curPos = articleText.indexOf( infoBox ) + infoBox.length();
-			int openBrackets = 1;
-			while( openBrackets > 0 )
-			{
-				int nextOpen = articleText.indexOf( openBracket, curPos );
-				int nextClose = articleText.indexOf( closeBracket, curPos );
-				if( nextOpen < nextClose )
-				{
-					curPos = nextOpen + openBracket.length();
-					++openBrackets;
-				}
-				else
-				{
-					curPos = nextClose + closeBracket.length();
-					--openBrackets;
-				}
-			}
-
-			// Grab everything immediately after the Infobox
-			return articleText.substring( curPos, articleText.length() );
-		}
-	}
-
-	private static class WikiImageListener implements Listener<JSONObject>, ErrorListener
-	{
-		private File             m_dataDirectory;
-		private NetworkImageView m_rocketImage;
-
-		public WikiImageListener( NetworkImageView rocketImage, File dataDirectory )
-		{
-			m_rocketImage = rocketImage;
-			m_dataDirectory = dataDirectory;
-		}
-
-		@Override
-		public void onResponse( JSONObject response )
-		{
-			Log.d( TAG, "Received wiki article data" );
-			if( response != null )
-			{
-				try
-				{
-					JSONObject parse = response.getJSONObject( "query" );
-					JSONArray pageIdsArray = parse.getJSONArray( "pageids" );
-
-					if( pageIdsArray.length() == 1 )
-					{
-						final String pageId = pageIdsArray.getString( 0 );
-
-						JSONObject pages = parse.getJSONObject( "pages" );
-						JSONObject rocketPage = pages.getJSONObject( pageId );
-
-						JSONObject rocketThumbnail = rocketPage.getJSONObject( "thumbnail" );
-						String rocketThumbnailUrl = rocketThumbnail.getString( "source" );
-
-						if( rocketThumbnailUrl != null )
-						{
-							Log.d( TAG, "Preparing to load rocket image: " + rocketThumbnailUrl );
-							ImageLoader imageLoader = new ImageLoader( TMinusApplication
-									                                           .getRequestQueue(), new DiskBitmapCache( m_dataDirectory, 10485760 ) );
-							m_rocketImage.setImageUrl( rocketThumbnailUrl, imageLoader );
-						}
-					}
-				}
-				catch( JSONException e )
-				{
-					e.printStackTrace();
-				}
-
-			}
-		}
-
-		@Override
-		public void onErrorResponse( VolleyError error )
-		{
-
-		}
-	}
-
-	private class RocketLoader extends AsyncTask<Integer, Void, Rocket>
+	private class RocketLoader extends AsyncTask<Integer, Void, Rocket> implements Listener, RocketDetailFetchListener
 	{
 		@Override
 		protected Rocket doInBackground( Integer... ids )
@@ -373,16 +179,54 @@ public class RocketDetailFragment extends DialogFragment
 		@Override
 		protected void onPostExecute( Rocket result )
 		{
-			Log.d( TAG, "Rocket details loaded." );
+			Log.i( TAG, "Rocket loaded." );
 			m_rocket = result;
 
-			if( m_rocket != null )
+			final Activity activity = getActivity();
+			if( m_rocket != null && activity != null )
 			{
-				requestRocketImage();
-				requestRocketArticle();
+				RocketDetailLoader detailLoader = new RocketDetailLoader( activity, this );
+				detailLoader.execute( m_rocket.id );
 
 				updateViews();
 			}
+		}
+
+		@Override
+		public void rocketDetailLoaded( RocketDetail rocketDetail )
+		{
+			m_rocketDetail = rocketDetail;
+			updateViews();
+		}
+
+		@Override
+		public void rocketDetailMissing( int rocketId )
+		{
+			final Activity activity = getActivity();
+			if( activity != null && m_rocket != null )
+			{
+				RocketDetailFetcher.requestRocketDetails( m_rocket, this, activity );
+			}
+		}
+
+		@Override
+		public void rocketDetailFetchSuccessful( int rocketId )
+		{
+			Log.i( TAG, "Rocket Detail fetch completely successfully for rocket id: " + rocketId );
+
+			final Activity activity = getActivity();
+			if( activity != null && m_rocket != null )
+			{
+				RocketDetailLoader detailLoader = new RocketDetailLoader( activity, this );
+				detailLoader.execute( m_rocket.id );
+			}
+		}
+
+		@Override
+		public void rocketDetailFetchFailed( int rocketId )
+		{
+			Log.w( TAG, "Rocket Detail fetch completely failed for rocket id: " + rocketId );
+			m_rocketSummary.setText( R.string.ROCKETDETAIL_no_summary );
 		}
 	}
 }
--- TMinus/src/main/java/com/darkrockstudios/apps/tminus/loaders/RocketDetailLoader.java
+++ TMinus/src/main/java/com/darkrockstudios/apps/tminus/loaders/RocketDetailLoader.java
@@ -16,56 +16,68 @@ import java.sql.SQLException;
  */
 public class RocketDetailLoader extends AsyncTask<Integer, Void, RocketDetail>
 {
-    public static interface Listener
-    {
-        public void rocketDetailLoaded( RocketDetail rocketDetail );
-        public void rocketDetailImageMissing();
-        public void rocketDetailSummaryMissing();
-    }
+	private static final String TAG = RocketDetailLoader.class.getSimpleName();
 
-    private static final String TAG = RocketDetailLoader.class.getSimpleName();
+	private Context                     m_context;
+	private RocketDetailLoader.Listener m_listener;
+	private int                         m_rocketId;
 
-    private Context m_context;
-    private RocketDetailLoader.Listener m_listener;
+	public RocketDetailLoader( Context context, RocketDetailLoader.Listener listener )
+	{
+		m_context = context;
+		m_listener = listener;
+	}
 
-    public RocketDetailLoader( Context context, RocketDetailLoader.Listener listener )
-    {
-        m_context = context;
-        m_listener = listener;
-    }
 
-    @Override
-    protected RocketDetail doInBackground( Integer[] ids )
-    {
-        RocketDetail rocketDetail = null;
+	@Override
+	protected RocketDetail doInBackground( Integer[] ids )
+	{
+		RocketDetail rocketDetail = null;
 
-        if( m_context != null )
-        {
-            final DatabaseHelper databaseHelper = OpenHelperManager.getHelper( m_context, DatabaseHelper.class );
-            if( databaseHelper != null )
-            {
-                try
-                {
-                    Dao<RocketDetail, Integer> rocketDetailDao = databaseHelper.getRocketDetailDao();
-                    rocketDetail = rocketDetailDao.queryForId( ids[ 0 ] );
+		m_rocketId = ids[ 0 ];
 
-                }
-                catch( SQLException e )
-                {
-                    e.printStackTrace();
-                }
+		if( m_context != null )
+		{
+			final DatabaseHelper databaseHelper = OpenHelperManager.getHelper( m_context, DatabaseHelper.class );
+			if( databaseHelper != null )
+			{
+				try
+				{
 
-                OpenHelperManager.releaseHelper();
-            }
-        }
+					Dao<RocketDetail, Integer> rocketDetailDao = databaseHelper.getRocketDetailDao();
+					rocketDetail = rocketDetailDao.queryForId( m_rocketId );
+				}
+				catch( SQLException e )
+				{
+					e.printStackTrace();
+				}
 
-        return rocketDetail;
-    }
+				OpenHelperManager.releaseHelper();
+			}
+		}
 
-    @Override
-    protected void onPostExecute( RocketDetail result )
-    {
-        Log.d(TAG, "RocketDetail loaded.");
-        m_listener.rocketDetailLoaded(result);
-    }
+		return rocketDetail;
+	}
+
+	@Override
+	protected void onPostExecute( RocketDetail result )
+	{
+		if( result != null )
+		{
+			Log.d( TAG, "RocketDetail loaded." );
+			m_listener.rocketDetailLoaded( result );
+		}
+		else
+		{
+			m_listener.rocketDetailMissing( m_rocketId );
+		}
+	}
+
+	public static interface Listener
+	{
+		public void rocketDetailLoaded( RocketDetail rocketDetail );
+
+		public void rocketDetailMissing( int rocketId );
+	}
+
 }
