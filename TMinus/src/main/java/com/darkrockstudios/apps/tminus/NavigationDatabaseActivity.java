e13b4eca3565900e26f465ab74398b4877443662
Wavesonics <adamwbrown@gmail.com>
2013-10-06 22:05:41 -0700

    More work on the Navigation Drawer

--- TMinus/src/main/java/com/darkrockstudios/apps/tminus/NavigationDatabaseActivity.java
+++ TMinus/src/main/java/com/darkrockstudios/apps/tminus/NavigationDatabaseActivity.java
@@ -5,13 +5,18 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Adam on 10/6/13.
@@ -35,7 +40,9 @@ public abstract class NavigationDatabaseActivity extends DatabaseActivity
 		m_drawerLayout.setDrawerListener( m_drawerToggle );
 
 		m_navigationList = (ListView) findViewById( R.id.NAVDRAWER_left_drawer );
-		m_navigationList.setAdapter( new NavigationListAdapter( this ) );
+		NavigationListAdapter navListAdapter =  new NavigationListAdapter( this );
+		m_navigationList.setOnItemClickListener( new NavigationItemClickListener() );
+		m_navigationList.setAdapter( navListAdapter );
 
 		ActionBar actionBar = getActionBar();
 		if( actionBar != null )
@@ -45,29 +52,81 @@ public abstract class NavigationDatabaseActivity extends DatabaseActivity
 		}
 	}
 
-	private class NavigationListAdapter extends ArrayAdapter<String>
+	private class NavigationItemClickListener implements AdapterView.OnItemClickListener
+	{
+		@Override
+		public void onItemClick( AdapterView<?> parent, View view, int position, long id )
+		{
+			final Intent intent;
+			switch( position )
+			{
+				case 0:
+					intent = new Intent( NavigationDatabaseActivity.this, LaunchListActivity.class );
+					intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+					break;
+				case 1:
+				case 2:
+				default:
+					intent = null;
+					break;
+			}
+
+			if( intent != null )
+			{
+				startActivity( intent );
+			}
+		}
+	}
+
+	private static class NavigationItem
+	{
+		public String   m_title;
+		public Drawable m_iconResource;
+
+		public NavigationItem( Context context, int titleResourceId, int iconResourceId )
+		{
+			m_title = context.getString( titleResourceId );
+			m_iconResource = context.getResources().getDrawable( iconResourceId );
+		}
+	}
+
+	private class NavigationListAdapter extends ArrayAdapter<NavigationItem>
 	{
 		public NavigationListAdapter( Context context )
 		{
 			super( context, R.layout.row_navigation_item, R.id.NAVDRAWER_nav_item_title );
+
+			NavigationItem launchListItem = new NavigationItem( context, R.string.NAVDRAWER_item_title_launch_list, R.drawable.ic_launch_type_planet_science );
+			add( launchListItem );
+
+			NavigationItem rocketListItem = new NavigationItem( context, R.string.NAVDRAWER_item_title_rocket_list, R.drawable.ic_launch_type_human_explore );
+			add( rocketListItem );
+
+			NavigationItem locationListItem = new NavigationItem( context, R.string.NAVDRAWER_item_title_location_list, R.drawable.ic_launch_type_earth_science );
+			add( locationListItem );
 		}
 
-		@Override
-		public int getCount()
+		public View getView(int position, View convertView, ViewGroup parent)
 		{
-			return 0;
-		}
+			final View row;
+			if( convertView == null )
+			{
+				row = getLayoutInflater().inflate( R.layout.row_navigation_item, parent, false );
+			}
+			else
+			{
+				row = convertView;
+			}
 
-		@Override
-		public String getItem( int position )
-		{
-			return "";
-		}
+			NavigationItem item = getItem( position );
 
-		@Override
-		public int getPosition( String item )
-		{
-			return super.getPosition( item );
+			TextView titleView = (TextView)row.findViewById( R.id.NAVDRAWER_nav_item_title );
+			titleView.setText( item.m_title );
+
+			ImageView iconView = (ImageView)row.findViewById( R.id.NAVDRAWER_nav_item_icon );
+			iconView.setImageDrawable( item.m_iconResource );
+
+			return row;
 		}
 
 		@Override
