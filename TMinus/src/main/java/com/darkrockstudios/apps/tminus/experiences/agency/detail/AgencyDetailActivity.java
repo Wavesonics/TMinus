097e7adee14754ed5730a407173f353106381bbb
Adam Brown <adamwbrown@gmail.com>
2014-02-15 04:08:05 -0800

    Added standard menu into Agency Detail Activity

--- TMinus/src/main/java/com/darkrockstudios/apps/tminus/experiences/agency/detail/AgencyDetailActivity.java
+++ TMinus/src/main/java/com/darkrockstudios/apps/tminus/experiences/agency/detail/AgencyDetailActivity.java
@@ -2,6 +2,7 @@ package com.darkrockstudios.apps.tminus.experiences.agency.detail;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -41,6 +42,13 @@ public class AgencyDetailActivity extends DatabaseActivity
 				                           .commit();
 			}
 		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu( final Menu menu )
+	{
+		getMenuInflater().inflate( R.menu.settings, menu );
+		return true;
 	}
 
 	@Override
